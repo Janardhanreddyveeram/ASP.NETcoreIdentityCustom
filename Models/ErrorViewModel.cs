26fcc57f6a000d87d97a755be130e174742ddb44
Janardhanreddyveeram <Janardhanreddy.veeram@equiniti.com>
2022-10-18 12:43:33 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NETcoreIdentityCustom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
